bde83a7a934828c6faefa8da1e741f707ba0bf0e
Davi Felix <davi.felix.lima@gmail.com>
2022-06-01 20:28:08 -0300

    remove migrations

--- migrations-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'dbname' => $_ENV["DATABASE_NAME"],
-    'user' => $_ENV["DATABASE_USER"],
-    'password' => $_ENV["DATABASE_PASSWORD"],
-    'host' => $_ENV["DATABASE_HOST"],
-    'driver' => 'pdo_mysql',
-];
